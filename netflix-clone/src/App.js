58fd95ec65c19ddc749efde040540cdf452e4b7d
shital@zestard.net <shital@zestard.net>
2020-10-03 01:54:05 +0530

    done with banner section

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./request";
+import Banner from './Banner'
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NATFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,56 @@
+import React, { useEffect,useState } from "react";
+import axios from "./axios";
+import requests from './request';
+import './banner.css';
+function Banner() {
+
+  const [movie,setMovie] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      console.log('fetchdata');
+      const req = await axios.get(requests.fetchNetflixOriginals);
+      console.log(req);
+      setMovie(
+        req.data.results[
+          Math.floor(Math.random()*req.data.results.length-1)
+        ]
+      );
+      console.log(req);
+      return req;
+    }
+    fetchData();
+  }, []);
+
+  
+  console.log(movie);
+  function truncateString(str, num) {
+    console.log(str);
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  }
+  return (
+    <header className='banner'
+      style={{
+        backgroundSize:'cover',
+        backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition:'center center'
+      }}
+    >
+      <div className='banner_contents'>
+        <h1 className='banner_title'>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className='banner_buttons'>
+          <button className='banner_button'>Play</button>
+          <button className='banner_button'>My List</button>
+          <h1 className='banner_description'>
+            {truncateString(movie?.overview,130)}</h1>
+        </div>
+      </div>
+      <div className='banner_fadebottom'/>
+    </header>
+  );
+}
+
+export default Banner;
